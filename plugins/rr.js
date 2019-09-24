1ffaed85f076451aa820c8d69b9efc36a832dd8e
Maribel Hearn <maribelhearn@disroot.org>
2019-09-24 23:58:01 +0200

    Fix RR 100% death rate, fix /online

--- plugins/rr.js
+++ plugins/rr.js
@@ -3,7 +3,7 @@
     ----------------------------------------------
     FUN COMMUNITY RUSSIAN ROULETTE rr.js
      - by Maribel Hearn, 2015-2015
-    
+
     This file contains the scripts necessary
     for Russian Roulette, a luck 'game' in
     which you try to survive your own shots.
@@ -12,7 +12,7 @@
     ----------------------------------------------
 */
 rr = sys.fileExists(DATA_FOLDER + "rr.txt") ? helpers.readObject("rr") : {};
-        
+
 rrcommands = {
     rrcommands: function (src, channel, command) {
         var commandsMessage = border;
@@ -28,9 +28,9 @@ rrcommands = {
         + border2;
         sys.sendHtmlMessage(src, commandsMessage, channel);
     }
-    
+
     ,
-    
+
     load: function (src, channel, command) {
         var name = sys.name(src), lower = name.toLowerCase();
         if (!rr[lower]) {
@@ -50,9 +50,9 @@ rrcommands = {
         helpers.saveData("rr");
         sys.sendHtmlAll(helpers.bot(bots.rr) + name + " has loaded a bullet into their revolver!", rrchannel);
     }
-    
+
     ,
-    
+
     unload: function (src, channel, command) {
         var name = sys.name(src), lower = name.toLowerCase();
         if (!rr[lower]) {
@@ -67,9 +67,9 @@ rrcommands = {
         helpers.saveData("rr");
         sys.sendHtmlAll(helpers.bot(bots.rr) + name + " has unloaded a bullet from their revolver!", rrchannel);
     }
-    
+
     ,
-    
+
     shoot: function (src, channel, command) {
         var name = sys.name(src), lower = name.toLowerCase(), random = sys.rand(0, 6);
         if (!rr[lower]) {
@@ -97,9 +97,9 @@ rrcommands = {
         }
         helpers.saveData("rr");
     }
-    
+
     ,
-    
+
     bullets: function (src, channel, command) {
         var lower = sys.name(src).toLowerCase(), grammar;
         if (!rr[lower]) {
@@ -109,9 +109,9 @@ rrcommands = {
         rr[lower].bullets == 1 ? grammar = "" : grammar = "s";
         sys.sendHtmlMessage(src, helpers.bot(bots.rr) + "You currently have " + rr[lower].bullets + " bullet" + grammar + " loaded.", rrchannel);
     }
-    
+
     ,
-    
+
     stats: function (src, channel, command) {
         var lower = sys.name(src).toLowerCase(), deathrate, message;
         if (!rr[lower]) {
@@ -119,6 +119,9 @@ rrcommands = {
             return;
         }
         deathrate = (rr[lower].deaths / rr[lower].shots * 100).toPrecision(2) + "%";
+        if (rr[lower].deaths == rr[lower].shots) {
+            deathrate = "100%";
+        }
         message = border + "<h2>Russian Roulette Stats</h2><br>"
         + "<br>"
         + "<b>Shots fired:</b> " + rr[lower].shots + "<br>"
@@ -129,9 +132,9 @@ rrcommands = {
         + "<br><timestamp/><br>" + border2;
         sys.sendHtmlMessage(src, message, rrchannel);
     }
-    
+
     ,
-    
+
     resetstats: function (src, channel, command) {
         var lower = sys.name(src).toLowerCase();
         if (!rr[lower]) {
