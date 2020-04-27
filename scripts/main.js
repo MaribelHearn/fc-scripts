7f8c18e6728db638715db8b1e2e23f8d2dbcf0f7
MaribelHearn <casbas.sj@gmail.com>
2020-04-27 18:05:51 +0200

    Fix auto-unban

--- scripts/main.js
+++ scripts/main.js
@@ -469,9 +469,9 @@
             if (banlist[index].time !== "-") {
                 banlist[index].time--;
                 if (banlist[index].time === 0) {
+                    sys.unban(index);
                     delete banlist[index];
                     members[index] ? name = members[index] : name = index;
-                    sys.unban(name);
                     sys.sendHtmlMain(helpers.bot(bots.ban) + name + "'s ban has expired!");
                 }
                 helpers.saveData("banlist");
